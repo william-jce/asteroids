8948f980d6ad316b3fb05971de06554349aacc46
william-jce <william.entschluss@gmail.com>
2025-09-22 15:51:50 +0900

    Add frame rate limit

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from constants import *
 
 def main():
     pygame.init
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     print("Starting Asteroids!")
     print(f'Screen width: {SCREEN_WIDTH}')
@@ -14,6 +16,7 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(60)/1000
 
 
 
