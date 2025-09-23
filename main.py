444b1745e70fdfd0850dd25d223b20415e005736
william-jce <william.entschluss@gmail.com>
2025-09-23 22:38:28 +0900

    Draw player triangle to screen

--- main.py
+++ main.py
@@ -1,23 +1,22 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    print("Starting Asteroids!")
-    print(f'Screen width: {SCREEN_WIDTH}')
-    print(f'Screen height: {SCREEN_HEIGHT}')
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60)/1000
-
 
 
 if __name__ == "__main__":
