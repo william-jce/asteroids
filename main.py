12aa72a73e43b9322d1350e490586a4f52296f22
william-jce <william.entschluss@gmail.com>
2025-09-24 22:04:41 +0900

    Add player rotation

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        player.update(dt)
         screen.fill("black")
         player.draw(screen)
         pygame.display.flip()
